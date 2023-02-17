1e32a2939096a07dc31a09ec8de7ee49d1255c02
HarrisonWismer <86372850+HarrisonWismer@users.noreply.github.com>
2023-02-17 15:54:43 -0800

    Update train.py

--- train.py
+++ train.py
@@ -15,8 +15,28 @@ for gpu in gpus:
 
 
 class DLV3Model():
+
+    """
+    Class containing functions to create the DeepLabV3+ Model as outlined:
+        - https://github.com/tensorflow/models/tree/master/research/deeplab
+
+    The code to create the model below was taken from:
+        - https://keras.io/examples/vision/deeplabv3_plus/
+
+    Inputs:
+        - image_size: An integer specifying the size of the images to be input and output from the model. For integer n specified, the images will be n x n.
+        - num_classes: An integer specifying the number of classes to be predicted.
+        - val_split: A float specifying the proportion of the total training data to be used for validation (0.0 - 1.0)
+        - batch_size: An integer specifying the batch size to be used for training.
+        - learning-rate: A float specifying the learning rate to be used when training.
+        - num_epochs: An integer specifying the number of epochs to train for.
+    """
     
     def __init__(self, image_size, num_classes, val_split, batch_size, learning_rate, num_epochs):
+        """
+        Instantiate the model with the correct image size and number of classes.
+        self.model will subsequently be updated when after calling train_model()
+        """
         self.image_size = image_size
         self.num_classes = num_classes
         self.val_split = val_split
@@ -27,6 +47,9 @@ class DLV3Model():
         self.model = self.DeeplabV3Plus(image_size,num_classes)
 
     def convolution_block(self,block_input, num_filters=256,kernel_size=3,dilation_rate=1,padding="same",use_bias=False,):
+        """
+        See: https://keras.io/examples/vision/deeplabv3_plus/
+        """
         x = layers.Conv2D(num_filters,
             kernel_size=kernel_size,
             dilation_rate=dilation_rate,
@@ -38,6 +61,9 @@ class DLV3Model():
 
 
     def DilatedSpatialPyramidPooling(self,dspp_input):
+        """
+        See: https://keras.io/examples/vision/deeplabv3_plus/
+        """
         dims = dspp_input.shape
         x = layers.AveragePooling2D(pool_size=(dims[-3], dims[-2]))(dspp_input)
         x = self.convolution_block(x, kernel_size=1, use_bias=True)
@@ -54,6 +80,9 @@ class DLV3Model():
         return output
     
     def DeeplabV3Plus(self,image_size, num_classes):
+        """
+        See: https://keras.io/examples/vision/deeplabv3_plus/
+        """
         model_input = keras.Input(shape=(image_size, image_size, 3))
         resnet50 = keras.applications.ResNet50(
             weights="imagenet", include_top=False, input_tensor=model_input
@@ -79,6 +108,18 @@ class DLV3Model():
         return keras.Model(inputs=model_input, outputs=model_output)
     
     def train_model(self, input_path):
+        """
+        Train the model using the specified training data.
+        
+        Input:
+            input_path: A path containing the folder with the training data. This path should lead to a filder, not a file.
+            This folder should contain two subfolders:
+                1. A folder called imgs containing the raw training images
+                2 A folder called masks containing the raw training masks
+
+        Output:
+            This function will print the final training and validation losses and accuracies.
+        """
         
         train_loader = DataLoader(input_path, self.image_size, self.batch_size, self.val_split)
         train_dataset, val_dataset = train_loader.load_training_data()
@@ -101,15 +142,34 @@ class DLV3Model():
         print("Validation Accuracy", validation_accuracy)
     
     def get_model(self):
+        """
+        Returns the current state of the model.
+        """
         return self.model
     
     def save_model(self, path):
+        """
+        Saves the current state of the model to the specified path.
+        
+        The path should be of the form '$\Path\ModelName'
+        since the model will be saved into its own folder (see Keras model.save() for more information)
+        """
         try:
             self.model.save(path)
         except:
             print("Invalid Path For Saving Model")
             
 class DataLoader():
+    """
+    The DataLoader classes handles the loading of the training data which is then input into the DLV3Model class for training.
+
+    Inputs:
+        input_path: The location of the training images
+        image_size: The size of the input and output images for the model.
+        batch_size: The specified batch size
+        val_split: The proportion of the training data to be used for validation purposes.
+
+    """
     
     def __init__(self,input_path, image_size, batch_size, val_split):
         self.image_size = image_size
@@ -118,6 +178,9 @@ class DataLoader():
         self.val_split = val_split
 
     def read_image(self,image_path, mask=False):
+        """
+        See: https://keras.io/examples/vision/deeplabv3_plus/
+        """
         image = tf.io.read_file(image_path)
         if mask:
             image = tf.image.decode_png(image, channels=1)
@@ -132,18 +195,30 @@ class DataLoader():
 
 
     def load_data(self,image_list, mask_list):
+        """
+        See: https://keras.io/examples/vision/deeplabv3_plus/
+        """
         image = self.read_image(image_list)
         mask = self.read_image(mask_list, mask=True)
         return image, mask
 
 
     def data_generator(self,image_list, mask_list):
+        """
+        See: https://keras.io/examples/vision/deeplabv3_plus/
+        """
         dataset = tf.data.Dataset.from_tensor_slices((image_list, mask_list))
         dataset = dataset.map(self.load_data, num_parallel_calls=tf.data.AUTOTUNE)
         dataset = dataset.batch(self.batch_size, drop_remainder=True)
         return dataset
     
     def load_training_data(self):
+        """
+        Creates a training and validation test split which is then subsequently made into
+        a Tensorflow dataset with the desited batch size.
+
+        Returns: train_dataset, val_dataset
+        """
         image_paths = sorted(glob(os.path.join(self.input_path, "imgs\\*")))
         mask_paths = sorted(glob(os.path.join(self.input_path, "masks\\*")))
 
@@ -161,6 +236,10 @@ class DataLoader():
 
 
 def get_opts():
+    """"
+    Handles the arguments needed to run the program. Note that the only non-optional arguments
+    are the path to the training data and the path to save the model to.
+    """
     parser = argparse.ArgumentParser(description = "Train A DeepLabV3+ Model Using Images & Masks")
     parser.add_argument('--imageSize', '-i', type=int, default = 256, help = "n x n Image Size To Downscale Images To. Default = 256")
     parser.add_argument('--numClasses', '-n', type=int, default = 2, help = "Number of Classes. Default = 2")
@@ -182,7 +261,6 @@ def main():
         print("Invalid Traning Path")
         exit(1)
     
-
     print("Creating Model")
     myDLV3 = DLV3Model(image_size = opts.imageSize, 
                    num_classes= opts.numClasses, 
