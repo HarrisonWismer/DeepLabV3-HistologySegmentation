8c0cdee0e927407e6fdef94a37eaf7e49892703a
HarrisonWismer <86372850+HarrisonWismer@users.noreply.github.com>
2023-02-27 16:43:39 -0800

    Changed from accuracy to F1 Scores

--- get_qc_metrics.py
+++ get_qc_metrics.py
@@ -6,29 +6,41 @@ import argparse
 from pathlib import Path
 
 def getQCMetrics(truth_image_path,prediction_image_path, num_classes):
-    
+    """
+    Given the paths to a truth label-image, a prediction label image, and the number of classes, output quality control metrics.
+
+    Input:
+        truth_image_path -> Path to an image with ground truth labels
+        prediction_image_path -> Path to an image with prediction labels
+        num_classes -> The total number of classes present (background (0) counts as a class))
+
+    Output:
+        An F1-Score for each class.
+    """
+    # Read in image paths into PILLOW images
     truth_image = PIL.Image.open(truth_image_path)
     prediction_image = PIL.Image.open(prediction_image_path)
-    
+
+    # If the masks are different sizes (one is downsampled etc.) resize the prediction image to the same size as the truth image
     if truth_image.size != prediction_image.size:
         print("Resizing Prediction Image of shape", prediction_image.size, "to Truth Image size of ", truth_image.size )
         prediction_image = prediction_image.resize(truth_image.size)
     
+    # Cast both to np arrays for easily manipulation
     truth_image = np.asarray(truth_image)
     prediction_image = np.asarray(prediction_image)
-    
-    image_size = prediction_image.size
-    
-    total_accuracy = (np.sum((truth_image == prediction_image)) / image_size) * 100
-    
-    val_dict = {curr_class : None for curr_class in range(1, num_classes)}
-    for curr_class in range(1, num_classes):
-        truth_class_image = truth_image == curr_class
-        prediction_class_image = prediction_image == curr_class
-        val_dict[curr_class] = (np.sum(truth_class_image == prediction_class_image) / image_size) * 100
-    
 
-    return total_accuracy, val_dict
+    val_dict = {curr_class : None for curr_class in range(1, num_classes)}
+
+    for curr_class in range(1,num_classes):
+        true_positives = (truth_image == curr_class) & (prediction_image == curr_class) & (truth_image == prediction_image)
+        false_positives = (prediction_image == curr_class) & (truth_image != curr_class)
+        false_negatives = (truth_image == curr_class) & (prediction_image != curr_class)
+
+        f1 = (2 * np.sum(true_positives)) / ((2*np.sum(true_positives)) + np.sum(false_positives) + np.sum(false_negatives))
+        val_dict[curr_class] = f1
+
+    return val_dict
 
 def get_opts():
     """"
@@ -48,18 +60,17 @@ def main():
     if not Path.exists(Path(opts.truth_path)):
         print("Cannot Read Ground Truth Image")
         exit(1)
+    
     if not Path.exists(Path(opts.prediction_path)):
         print("Cannot Read Prediction Image")
         exit(1)
 
-    total_accuracy, val_dict = getQCMetrics(opts.truth_path,opts.prediction_path, opts.num_classes)
+    print()
+    val_dict = getQCMetrics(opts.truth_path,opts.prediction_path, opts.num_classes)
     
     print()
-    print()
     print("------------------")
-    print("Overall Accuracy:", total_accuracy)
-    print()
-    print("Class Accuracies:")
+    print("Class F1 Scores:")
     print("------------------")
     if opts.class_names is None:
         for class_value in val_dict:
